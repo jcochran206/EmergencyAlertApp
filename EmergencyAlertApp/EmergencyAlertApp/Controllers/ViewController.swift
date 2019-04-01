06a3f4b85b0ef48a6d1975c61c35a0811cf494a1
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-04-01 08:49:50 -0700

    added mapview, userlocation, and button to add events

---
+++ EmergencyAlertApp/EmergencyAlertApp/Controllers/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  EmergencyAlertApp
+//
+//  Created by Jonathan Cochran on 3/31/19.
+//  Copyright © 2019 Jonathan Cochran. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var eventBtn: UIButton!
+    
+    
+    private lazy var locationManager: CLLocationManager = {
+       
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = kCLDistanceFilterNone
+        manager.requestAlwaysAuthorization()
+        return manager
+        
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
+        setupUI()
+    }
+
+    private func setupUI() {
+        self.eventBtn.layer.cornerRadius = 8.0
+        self.eventBtn.layer.masksToBounds = true
+    }
+}
+
